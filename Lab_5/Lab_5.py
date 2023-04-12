bf97748fa602ec5d745aaca3522e6722bd7518b0
Kyrylo Boiko <kyryl.boiko@gmail.com>
2023-04-12 21:06:53 +0300

    Update Lab_5.py

--- Lab_5/Lab_5.py
+++ Lab_5/Lab_5.py
@@ -1,24 +1,23 @@
 class Sort():
     def Merge(self, Array, start, middle, end):
-        if start <= middle and middle < end:
-            n1 = middle - start + 1
-            n2 = end - middle
-            Left = [0] * (n1 + 1)
-            Right = [0] * (n2 + 1)
-            for i in range(n1):
-                Left[i] = Array[start+i]
-            for j in range(n2):
-                Right[j] = Array[middle+j+1]
-            Left[n1] = float('inf')
-            Right[n2] = float('inf')
-            i = j = 0
-            for k in range(start, end+1):
-                if Left[i] <= Right[j]:
-                    Array[k] = Left[i]
-                    i += 1
-                else:
-                    Array[k] = Right[j]
-                    j += 1
+        n1 = middle - start + 1
+        n2 = end - middle
+        Left = [0] * (n1 + 1)
+        Right = [0] * (n2 + 1)
+        for i in range(n1):
+            Left[i] = Array[start+i]
+        for j in range(n2):
+            Right[j] = Array[middle+j+1]
+        Left[n1] = float('inf')
+        Right[n2] = float('inf')
+        i = j = 0
+        for k in range(start, end+1):
+            if Left[i] <= Right[j]:
+                Array[k] = Left[i]
+                i += 1
+            else:
+                Array[k] = Right[j]
+                j += 1
 
     def MergeSort(self, Array, start, end):
         if start < end:
@@ -29,8 +28,7 @@ class Sort():
         return Array
 
 sort = Sort()
-array = input('Введіть елементи масиву через пробіл: ').split()
-array = [int(x) for x in array]
-print(array)
+array = [int(i) for i in input("Введіть елементи масиву через пробіл:").split()]
+print(*array)
 result = sort.MergeSort(array, 0, len(array)-1)
-print(result)
+print(*result)
